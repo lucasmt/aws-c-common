4b1502ca0a9ae74efe00977b1b2ef61143446cc5
Lucas M. Tabajara <lucasmt@amazon.com>
2019-06-05 19:29:25 -0400

    Proof harness for aws_byte_cursor_read_be* functions
    * Added proof harness for aws_byte_cursor_read_be16, aws_byte_cursor_read_be32 and aws_byte_cursor_read_be64
    * Added pre and postconditions to aws_byte_cursor_read_be16, aws_byte_cursor_read_be32 and aws_byte_cursor_read_be64
    * Added a preprocessor check to aws_hton64 to ignore optimizations when using CBMC

--- include/aws/common/byte_buf.h
+++ include/aws/common/byte_buf.h
@@ -733,12 +733,15 @@ AWS_STATIC_IMPL bool aws_byte_cursor_read_u8(struct aws_byte_cursor *AWS_RESTRIC
  * cursor unchanged.
  */
 AWS_STATIC_IMPL bool aws_byte_cursor_read_be16(struct aws_byte_cursor *cur, uint16_t *var) {
+    AWS_PRECONDITION(aws_byte_cursor_is_valid(cur));
+    AWS_PRECONDITION(AWS_OBJECT_PTR_IS_WRITABLE(var));
     bool rv = aws_byte_cursor_read(cur, var, 2);
 
     if (AWS_LIKELY(rv)) {
         *var = aws_ntoh16(*var);
     }
 
+    AWS_POSTCONDITION(aws_byte_cursor_is_valid(cur));
     return rv;
 }
 
@@ -751,12 +754,15 @@ AWS_STATIC_IMPL bool aws_byte_cursor_read_be16(struct aws_byte_cursor *cur, uint
  * cursor unchanged.
  */
 AWS_STATIC_IMPL bool aws_byte_cursor_read_be32(struct aws_byte_cursor *cur, uint32_t *var) {
+    AWS_PRECONDITION(aws_byte_cursor_is_valid(cur));
+    AWS_PRECONDITION(AWS_OBJECT_PTR_IS_WRITABLE(var));
     bool rv = aws_byte_cursor_read(cur, var, 4);
 
     if (AWS_LIKELY(rv)) {
         *var = aws_ntoh32(*var);
     }
 
+    AWS_POSTCONDITION(aws_byte_cursor_is_valid(cur));
     return rv;
 }
 
@@ -769,12 +775,15 @@ AWS_STATIC_IMPL bool aws_byte_cursor_read_be32(struct aws_byte_cursor *cur, uint
  * cursor unchanged.
  */
 AWS_STATIC_IMPL bool aws_byte_cursor_read_be64(struct aws_byte_cursor *cur, uint64_t *var) {
+    AWS_PRECONDITION(aws_byte_cursor_is_valid(cur));
+    AWS_PRECONDITION(AWS_OBJECT_PTR_IS_WRITABLE(var));
     bool rv = aws_byte_cursor_read(cur, var, sizeof(*var));
 
     if (AWS_LIKELY(rv)) {
         *var = aws_ntoh64(*var);
     }
 
+    AWS_POSTCONDITION(aws_byte_cursor_is_valid(cur));
     return rv;
 }
 
